761e6a38f8c0149f13738cc7aadcdf61f69b0dcc
David Davis <davidjamesdavis.djd@gmail.com>
2018-10-13 04:08:32 -0700

    added post deployment script scale

--- post-deployment.sh
+++ post-deployment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# deploy to url and then alias
+# deploy to url and then alias, followed by setting scale to at least 1 so it doesn't sleep
+# See https://zeit.co/docs/guides/app-lifecycle-and-scalability
 now && now alias && now scale marmt.io 1
